096ce4f79a5078ca43a37a3996233f442f3a9d3b
Alex Rudakov <ayttewcommits@gmail.com>
2015-06-17 19:04:37 +0300

    :/

--- checks.py
+++ checks.py
@@ -5,21 +5,21 @@ host = 'http://localhost:5000'
 def login(username, password):
 	data = {'username' : username, 'password' : password}
 	url = host + '/login'
-	rq = requests.post(url, json = data)
+	reques = requests.post(url, json = data)
 	print (rq.content)
-	return rq
+	return reques
 
 def reg(username, password):
 	data = {'username': username, 'password' : password}
 	url = host + '/register'
-	rq = requests.post(url, json = data)
-	print (rq.content)
-	return rq
+	reques = requests.post(url, json = data)
+	print (reques.content)
+	return reques
 
 def up(username, password, path):
 	file = {'file' : open(path, mode='r')}
 	data = {'username' : username, 'password' : password}
 	url = host + '/upload'
-	rq = requests.post(url, data = data, files = file)
-	print (rq.content)
-	return rq
+	reques = requests.post(url, data = data, files = file)
+	print (reques.content)
+	return reques
--- curlyserver.py
+++ curlyserver.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-    flash
+    flash, render_template
 from werkzeug.utils import secure_filename
 from contextlib import closing
 from werkzeug import utils, secure_filename
@@ -20,6 +20,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('CURLYSERV_CONF', silent=True)
 # Connection to database
+
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
@@ -47,6 +48,13 @@ def get_space(username):
     used_space = used_space.fetchone()[0]
     return (space - used_space)
 
+def get_files(username):
+    files = os.listdir(os.path.join(app.config['STORAGE'], username))
+    if not files:
+        return False
+    else:
+        return files
+
 # Check if you can upload file
 def allowed_file(filename):
     return '.' in filename and \
@@ -70,20 +78,86 @@ def upload():
             used_space = used_space + file_length
             db.execute('update users set used_storage = (?) where username = (?)', [used_space, username])
             db.commit()
-            return 'Success!'
+            return 'OK'
         else:
-            return 'You do not have enough space!'
+            return 'ME'
     else:
-        return 'Error authorization'
+        return 'AE'
 
-# Check if user is root
-@app.route('/root_login', methods = ['POST'])
+
+# Functions for administration purposes
+@app.route('/')
+def show_users():
+    db = connect_db()
+    cur = db.execute('select username, storage, used_storage from users order by id asc')
+    users = [dict(username=row[0], storage=row[1], used_storage=row[2]) for row in cur.fetchall()]
+    return render_template('show_users.html', users=users)
+
+def change_space(username, new_value):
+    db = connect_db()
+    db.execute('update users set storage = (?) where username = (?)', [new_value, username])
+
+@app.route('/change_space/<username>', methods=['POST'])
+def change(username):
+    db = connect_db()
+    value = int(request.form['bytes'])
+    db.execute('update users set storage = (?) where username = (?)', [value, username])
+    return redirect(url_for('show_users'))
+
+# Administrator login function
+@app.route('/root_login', methods=['GET', 'POST'])
 def root_login():
-    
-	if username == app.config['ROOT_USERNAME'] and password == app.config['ROOT_PASSWORD']:
-		return 
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['ROOT_USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['ROOT_PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in as admin')
+            return redirect(url_for('show_users'))
+    return render_template('login.html', error=error)
+
+# Administrator logout function
+@app.route('/root_logout')
+def root_logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_users'))
+
+# Add new user through the form
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    uname = request.form['username']
+    db = connect_db()
+    try:
+        db.execute('insert into users (username, password) values (?, ?)',
+            [uname, request.form['password']])
+        db.commit()
+        os.mkdir(os.path.join(app.config['STORAGE'], uname))
+        return redirect(url_for('show_users'))
+    except sqlite3.IntegrityError:
+        flash('This username already exists')
+        return redirect(url_for('show_users'))
+
+@app.route('/show_files', methods=['POST'])
+def show_files():
+    us = request.form['user']
+    if not get_files(us):
+        return render_template('show_files.html', [])
     else:
-        flash ('Wrong username')
+        return render_template('show_files.html', get_files(us))
+
+# Remove existing user
+@app.route('/remove_user', methods=['POST'])
+def remove_user():
+    uname = request.form['username']
+    db = connect_db()
+    db.execute('delete from users where (username) = (?)', uname)
+    db.commit()
+    shutil.rmtree(os.path.join(app.config['STORAGE'], uname))
+    return redirect(url_for('show_users'))
 
 # Main login function
 @app.route('/login', methods=['GET', 'POST'])
@@ -138,6 +212,70 @@ def logged_in():
         return 'Yes'
     else:
         return 'No'
+
+# interfaces for contacting with client
+# Sign in
+@app.route('/signin', methods=['POST'])
+def Nsignin():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    db = connect_db()
+    if try_login(username, password):
+        return True
+    else:
+        return False
+
+# Get list of files
+@app.route('/listoffiles', methods=['GET', 'POST'])
+def Nlistoffiles():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if try_login(username, password):
+        if get_files(username) == []:
+            return 'No files'
+        else:
+            l = get_files(username)
+            for a in l:
+                print a
+            return 'List of all files is above.'
+
+
+@app.route('/download', methods=['POST'])
+def Ndownload():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    file = request.args.get('file')
+
+    if try_login(username, password):
+        return send_from_directory(os.path.join(app.config['STORAGE'], username), file)
+    else:
+        return 'AE'
+
+@app.route('/remove', methods=['POST'])
+def Nremove():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    filename = request.form.get('filename')
+    
+    db = connect_db()
+
+    path = os.path.join(app.config['STORAGE'], username)
+    path = os.path.join(path, filename) 
+    
+    filesize = os.stat(path)
+    filesize = filesize.st_size
+
+    if try_login(username, password):
+        if os.path.exists(path):
+            os.remove(path)
+            used_space = db.execute('select used_storage from users where username = (?)', [username])
+            used_space = used_space.fetchone()[0]
+            used_space = used_space + filesize
+            db.execute('update users set used_storage = (?) where username = (?)', [filesize, username])
+            db.commit()
+        return 'No such file'
+    return 'Login is incorrect'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
